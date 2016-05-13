dbb8171da2b9f4d01d90156ba2ec7ac93949b1a5
hefesto <daniel.herrera.rendon@gmail.com>
2016-05-13 13:20:34 -0500

    ya muestra todas las páginas, ahora sólo falta ligarlas.

--- Server.js
+++ Server.js
@@ -12,11 +12,27 @@ app.use(express.static('public'));
 
 router.get("/",function(req,res){
     // console.log("request address: " + req.connection.remoteAddress)
-    res.sendFile(path + "index.html");
+    res.sendFile(path + "home_2.html");
 });
 
-router.get("/posts",function(req,res){
-    res.sendFile(path + "posts.html");
+router.get("/bd",function(req,res){
+    res.sendFile(path + "page_bd.html");
+});
+
+router.get("/lang",function(req,res){
+    res.sendFile(path + "page_lang.html");
+});
+
+router.get("/deps",function(req,res){
+    res.sendFile(path + "page_deps.html");
+});
+
+router.get("/deps_tree",function(req,res){
+    res.sendFile(path + "page_depstree.html");
+});
+
+router.get("/deps_tree2",function(req,res){
+    res.sendFile(path + "page_depstree_2.html");
 });
 
 app.use("/",router);
